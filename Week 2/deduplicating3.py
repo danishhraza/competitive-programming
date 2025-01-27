8a412457fe2f4e809ec142aa25fe366e5e044b7a
Danish Raza <danishh.raza05@gmail.com>
2025-01-28 01:40:03 +0500

    deduplicating week 2

---
+++ Week 2/deduplicating3.py
@@ -0,0 +1,38 @@
+while True:
+    n = int(input().strip())
+    if n == 0:
+        break
+
+    files = [input() for _ in range(n)]
+
+    xor_hashes = []
+    for f in files:
+        result = 0
+        for ch in f:
+            result ^= ord(ch)
+        xor_hashes.append(result)
+
+    hash_map = {}         
+    collision_count = 0
+    unique_files = 0
+
+    for i, h in enumerate(xor_hashes):
+        if h not in hash_map:
+            hash_map[h] = [i]
+            unique_files += 1
+        else:
+            collisions_for_this_file = 0
+            matched = False
+            for prev_idx in hash_map[h]:
+                if files[i] != files[prev_idx]:
+                    collisions_for_this_file += 1
+                else:
+                    matched = True
+            collision_count += collisions_for_this_file
+
+            if not matched:
+                unique_files += 1
+
+            hash_map[h].append(i)
+
+    print(unique_files, collision_count)
